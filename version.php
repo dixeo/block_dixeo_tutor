87245f318469064873998b05a1c58256fccd0fa1
Josemaria Bolanos <josemabol@gmail.com>
2026-05-20 20:29:23 -0400

    Implement unread indicator

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026072201;
+$plugin->version   = 2026072202;
 $plugin->requires  = 2024100700;
 $plugin->component = 'block_dixeo_tutor';
 $plugin->maturity  = MATURITY_STABLE;
