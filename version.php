5ed5de565ebb4bd8a8f3b85018dae6198eb9dabb
Josemaria Bolanos <josemabol@gmail.com>
2026-07-21 14:32:03 -0400

    Add release 4.2.2

--- version.php
+++ version.php
@@ -28,11 +28,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026072000;
+$plugin->version   = 2026072100;
 $plugin->requires  = 2024100700;
 $plugin->component = 'block_dixeo_tutor';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '4.2.1';
+$plugin->release   = '4.2.2';
 $plugin->dependencies = [
-    'local_dixeo' => 2026071400,
+    'local_dixeo' => 2026072100,
 ];
